61de1a11d25d2d7bad0d3be60ed11c84ae7b7c6e
Matthew Cowley <matthewcowley@Matthews-MacBook-Air.local>
2020-09-19 16:47:52 -0400

    fixed onclick input

--- assets/landing.js
+++ assets/landing.js
@@ -1,23 +1,22 @@
 // local storage
 var nameInsert = document.getElementById("name");
 var emailInsert = document.getElementById("email");
-var btnInsert = document.getElementById("btn");
 
-btnInsert.onclick = function () {
+
+
+document.getElementById('myform').addEventListener('submit', function(e) {
+  e.preventDefault()
   var key = nameInsert.value;
   var value = emailInsert.value;
-
+  
   console.log(key);
   console.log(value);
 
+  
+
   if (key && value) {
     localStorage.setItem(key, value);
-    location.reload();
+    window.location.href = "main.html"
   }
-};
+});
 
-// when submit enters main page
-function myOnClickFn(event) {
-  event.preventDefault();
-  document.location.replace = "./main.html";
-}
