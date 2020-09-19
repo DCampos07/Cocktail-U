c11140a53d2228de77c087f7a5720c45c51ef823
Matthew Cowley <matthewcowley@Matthews-MacBook-Air.local>
2020-09-19 13:21:16 -0400

    localStorage set

---
+++ assets/landing.js
@@ -0,0 +1,18 @@
+// local storage
+var nameInsert = document.getElementById("name");
+var emailInsert = document.getElementById("email");
+var btnInsert = document.getElementById("btn");
+
+btnInsert.onclick = function () {
+  var key = nameInsert.value;
+  var value = emailInsert.value;
+
+  console.log(key);
+  console.log(value);
+
+  if (key && value) {
+      localStorage.setItem(key, value);
+      location.reload();
+  }
+};
+
